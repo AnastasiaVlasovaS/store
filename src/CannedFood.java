fac6ad9f0659bed368dbd8e3656416f26acff117
AnastasiaVlasovaS <a79373650726@icloud.com>
2025-09-03 10:54:07 +0500

    Update CannedFood.java

--- src/CannedFood.java
+++ src/CannedFood.java
@@ -1,5 +1,14 @@
 public class CannedFood extends Product {
-    public CannedFood(String name, double price, String manufacturer) {
+    private static final int DEFAULT_SHELF_LIFE_MONTHS = 24;
+    private String expirationDate;
+
+     public CannedFood(String name, double price, String manufacturer, String expirationDate) {
         super(name, price, manufacturer);
+        this.expirationDate = expirationDate;
+    }
+    @Override
+    public String toString() {
+         return super.toString() + "(Истекает: " + expirationDate + ", Стандартный срок хранение: "
+                 + DEFAULT_SHELF_LIFE_MONTHS + " месяцев)";
     }
 }
