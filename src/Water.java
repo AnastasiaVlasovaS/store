61bad2dff743106722a61e9caad24a93db7d5c1b
AnastasiaVlasovaS <a79373650726@icloud.com>
2025-09-03 11:34:44 +0500

    Update Water.java

--- src/Water.java
+++ src/Water.java
@@ -2,10 +2,17 @@ class Water extends Product {
     private double volume;
     private static final double DEFAULT_VOLUME = 1.5;
 
-    public Water(String name, double price, String manufacturer, double volume) {https://github.com/AnastasiaVlasovaS/store/blob/main/src/Water.java
+    public Water(String name, double price, String manufacturer, double volume) {
         super(name, price, manufacturer);
         this.volume = volume;
     }
+
+    public Water(String name, double price, String manufacturer) {
+        super(name, price, manufacturer);
+        this.volume = DEFAULT_VOLUME;
+
+    }
+
     @Override
     public String toString() {
         return super.toString() + " (Объем: " + volume + "л., Стандартный объем: " + DEFAULT_VOLUME + "л.)";
